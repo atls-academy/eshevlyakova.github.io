622740cf51021815fa0f875511751c8bcf9b9952
Евгения <euschevlyakova@yandex.ru>
2022-11-08 16:48:41 +0300

    fix: smoothness of the backdrop in drawer

--- ui/drawer/src/backdrop/backdrop.styles.ts
+++ ui/drawer/src/backdrop/backdrop.styles.ts
@@ -2,7 +2,6 @@ import { styleFn } from 'styled-system'
 
 export const baseBackdropStyles: styleFn = ({ theme }) => ({
   backgroundColor: theme.colors.drawer.backdrop,
-  backdropFilter: 'blur(12px)',
   width: '100%',
   height: '100%',
   cursor: 'pointer',
--- ui/drawer/src/drawer.component.tsx
+++ ui/drawer/src/drawer.component.tsx
@@ -20,9 +20,9 @@ const Drawer: FC<DrawerProps> = ({ active, onClose }) => (
   <Renderer active={active}>
     <motion.div
       style={{ position: 'fixed', left: 0, top: 0, width: '100%', height: '100%', zIndex: 900 }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      initial={{ opacity: 0, backdropFilter: 'blur(0px)' }}
+      animate={{ opacity: 1, backdropFilter: 'blur(12px)' }}
+      exit={{ opacity: 0, backdropFilter: 'blur(0px)' }}
       transition={{ duration: 0.5 }}
     >
       <Backdrop onClick={onClose} />
